02a4da5c53a874d1f0655fc3c8a9b9161a5d7edf
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-03-19 15:43:37 +1100

    Add Fox and Vasseur 2008 model (via Georges)

---
+++ R/fox-2008.R
@@ -0,0 +1,101 @@
+## Extending this model to multiple types (i.e., more than two) is
+## going to be hard, because the 'y' matrix (here, 'Y') is as much of
+## a property of the species as 'u' (the trait that can evolve).  But
+## 'Y' needs expanding out to be a length(R) * length(u) matrix,
+## rather than just being 2 * 2.  Georges is working on this.
+##
+## At present, parameters follow the paper, and defaults are as given
+## in the paper.  The number of resources is fixed at two.
+##
+## The death rates 'd' is also a property of the species, at least in
+## the general case.
+
+##' Model implementing "Character Convergence under Competition for
+##' Nutritionally Essential Resources.", Fox and Vasseur, The American
+##' Naturalist, 2008.
+##'
+##' Test case...
+##' @title Fox and Vasseur 2008
+##' @param Y See paper
+##' @param d See paper
+##' @param D See paper
+##' @param S See paper
+##' @author Georges Kunstler and Rich FitzJohn
+##' @export
+make_fox_2008 <- function(Y=rbind(c(0.5, 1), c(1, 0.5)),
+                          d=0.1, D=0.1, S=c(1, 1)) {
+  defaults <- list(Y=Y, d=d, D=D, S=S)
+  parameters <- make_parameters(defaults, environment())
+
+  dydt <- function(t, ode.y, x, ...) {
+    y <- ode.y[1:2]
+    R <- ode.y[3:4]
+    gj <- g(x, R)
+    dRdt <- D * (S - R) - colSums(y * gj / Y)
+    dydt <- y * (gj - d)
+    c(dydt, dRdt)
+  }
+  dydt.deSolve <- function(...) {
+    list(dydt(...))
+  }
+
+  ## Given traits 'x' and initial densities 'y', compute the
+  ## equilibrium.  Return the equilibrium resource availability at the
+  ## same time.
+  equilibrium <- function(x, y, method="runsteady",
+                          init_time=200,
+                          max_time=1e5) {
+    # Initial resources are set to 'S'
+    y0 <- c(y, S)
+    if (init_time > 0) {
+      y0 <- unname(lsoda(y0, c(0, init_time), dydt.deSolve, x)[2,-1])
+    }
+    
+    method <- match.arg(method, c("runsteady", "nleqslv", "simple"))
+    if (method == "runsteady") {
+      ans <- runsteady(y0, dydt.deSolve, x, times=c(0, Inf),
+                       hmax=1)$y
+    } else if (method == "nleqslv") {
+      ans <- nleqslv(y0, function(y) dydt(0, y, x), global="none")$x
+    } else if (method == "simple") {
+      ans <- unname(lsoda(y0, c(init_time, max_time), dydt.deSolve, x)[2,-1])
+    }
+
+    list(y=ans[1:2], R=ans[3:4])
+  }
+
+  run <- function(x, y, times) {
+    y0 <- c(y, S)
+    res <- lsoda(y0, times, dydt.deSolve, x)
+    list(t=res[,1],
+         y=res[,2:3],
+         R=res[,4:5])
+  }
+
+  g <- function(x, R) {
+    pmin(Y[1,] * x       * R[1],
+         Y[2,] * (1 - x) * R[2])
+  }
+
+  # Analytic solutions:
+  # Equilibrium resource density when limiting
+  analytic.resource.limiting <- function(x, sp=1) {
+    cbind(d / (Y[1,sp] * x),       # B1a, B1c
+          d / (Y[2,sp] * (1 - x))) # B1b, B1d
+  }
+  # Equilibrium resource density when not limiting
+  analytic.resource.nonlimiting <- function(x, sp=1) {
+    cbind(1 - Y[2,sp]/Y[1,sp] + d / (Y[1,sp] * (1 - x)),
+          1 - Y[1,sp]/Y[2,sp] + d / (Y[2,sp] * x))
+  }
+  analytic <- list(resource=list(
+                     limiting=analytic.resource.limiting,
+                     nonlimiting=analytic.resource.nonlimiting))
+
+  ret <- list(equilibrium  = equilibrium,
+              run          = run,
+              parameters   = parameters,
+              analytic     = analytic)
+  class(ret) <- "model"
+  ret
+}
---
+++ scripts/run_fox_2008.R
@@ -0,0 +1,107 @@
+library(rootSolve)
+library(Revolve)
+library(deSolve)
+library(plyr)
+
+xx <- c(0.2, 0.7) # same as Georges
+yy <- c(0.5, 0.5)
+tt <- seq(0, 300, length=301)
+
+m <- make_fox_2008()
+
+## 1. At an arbitrarily chosen set of initial densities and states,
+## here is the approach to equilibrium, with the values from runsteady
+## added.
+obj.eq <- m$equilibrium(xx, yy)
+obj.tr <- m$run(xx, yy, tt)
+
+op <- par(mfrow=c(2, 1), mar=c(4.1, 4.1, .5, .5))
+matplot(obj.tr$t, obj.tr$y, type="l", xlab="", ylab="Abundance")
+points(rep(max(tt), 2), obj.eq$y, col=1:2)
+matplot(obj.tr$t, obj.tr$R, type="l", xlab="Time", ylab="Resource")
+points(rep(max(tt), 2), obj.eq$R, col=1:2)
+par(op)
+
+## Now, look at the equilibrium of the single species setup.  For this
+## we'll just start the second species at a density of 0 and with an
+## arbitrary 0.5 for their state.
+yy[2] <- 0
+xx[2] <- 0.5
+xx[1] <- 0.4
+
+obj.eq <- m$equilibrium(xx, yy)
+obj.tr <- m$run(xx, yy, tt)
+
+op <- par(mfrow=c(2, 1), mar=c(4.1, 4.1, .5, .5))
+matplot(obj.tr$t, obj.tr$y, type="l", xlab="", ylab="Abundance",
+        ylim=c(0, 1))
+points(rep(max(tt), 2), obj.eq$y, col=1:2)
+matplot(obj.tr$t, obj.tr$R, type="l", xlab="Time", ylab="Resource",
+        ylim=c(0, 1))
+points(rep(max(tt), 2), obj.eq$R, col=1:2)
+par(op)
+
+## Single species equilibrium helper function.
+eq1 <- function(x1, m, y1=0.5, ...) {
+  x <- c(x1, 0.5) # arbitrarily 0.5 for absent species
+  y <- c(y1, 0)
+  m$equilibrium(x, y, ...)
+}
+## Over a range of trait values
+xx <- seq(0, 1, length=101)
+obj <- lapply(xx, eq1, m)
+eq.R <- laply(obj, "[[", "R")
+eq.y <- laply(obj, "[[", "y")
+
+## Colours for resources 1 and 2:
+col <- c("red", "blue")
+col2 <- c("#ff000055", "#0000ff55")
+
+matplot(xx, m$analytic$resource$limiting(xx), type="l",
+        ylim=c(0, 1), lty=1, col=col)
+matlines(xx, m$analytic$resource$nonlimiting(xx), lty=2, col=col)
+alive <- aaply(eq.y, 1, max) > 1e-5
+points(xx, eq.R[,1], pch=ifelse(alive, 19, 1), col=col2[1])
+points(xx, eq.R[,2], pch=ifelse(alive, 19, 1), col=col2[2])
+
+## Two species equilibrium helper function
+eq2 <- function(x, m, y=c(.5, .5), ...) {
+  m$equilibrium(x, y, ...)
+}
+
+# This is terribly slow at the moment.  No effort taken to speed it
+# up.
+x2 <- as.matrix(expand.grid(x1=xx, x2=xx))
+res <- alply(x2, 1, eq2, m, .progress="text")
+
+y2 <- laply(res, "[[", "y")
+
+# Different types at equilibrium.
+z2 <- y2 > 1e-6
+type <- z2[,1] * 2 + z2[,2]
+image(xx, xx, matrix(type, nrow=length(xx)))
+abline(1, -1)
+
+# Lines from the paper -- they don't match up exactly because of
+# things like numerical issues.  Still have to get to the bottom of
+# that.
+extra <- function(x, m) {
+  p <- m$parameters$get()
+  d <- p$d
+  Y <- p$Y
+  S <- p$S
+
+  data.frame(B3a=d * Y[1,2] / (d * Y[1,1]) * x,
+             B3b=1 - d * Y[2,1] / (d * Y[2,2]) * (1 - x),
+             B5a=d * Y[1,2] / (Y[1,1] * (Y[1,2] * S[1] -
+               Y[2,2] * S[2] + d / (1 - x))),
+             B5b=1 - d * Y[2,1] / (Y[2,2] * (Y[2,1] * S[2] -
+               Y[1,1] * S[1] + d / x)))
+}
+
+tmp <- extra(xx, m)
+
+lines(tmp$B3a, xx)
+lines(xx, tmp$B3b)
+lines(tmp$B5a, xx)
+lines(xx, tmp$B5b)
