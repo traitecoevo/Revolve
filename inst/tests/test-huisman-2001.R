cc264a816f77c1cf1adb436b50d25057b4224c4f
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-04-10 12:28:08 +1000

    Separate out the matrices from the model code.

---
+++ inst/tests/test-huisman-2001.R
@@ -0,0 +1,26 @@
+source("helper-Revolve.R")
+
+context("Huisman and Weissing 1999")
+
+test_that("Model parameters", {
+  m <- make_huisman_2001()
+
+  defaults <- list(r=1, m=1/4, D=1/4, S=c(1, 1))
+  expect_that(names(m$parameters), equals(names(defaults)))
+  expect_that(m$parameters$get(),  equals(defaults))
+})
+
+test_that("Model components", {
+  m <- make_huisman_2001()
+  expect_that(names(m), equals(c("equilibrium", "run", "parameters",
+                                 "n", "k", "K", "C", "p", "Rstar")))
+  expect_that(m$equilibrium, is_a("function"))
+  expect_that(m$run,         is_a("function"))
+  expect_that(m$parameters,  is_a("parameters"))
+  expect_that(m$n,           is_identical_to(2L))
+  expect_that(m$k,           is_identical_to(2L))
+  expect_that(m$K,           is_identical_to(huisman_mat_2))
+  expect_that(m$C,           is_identical_to(huisman_mat_2))
+  expect_that(m$p,           is_a("function"))
+  expect_that(m$Rstar,       is_a("function"))
+})
